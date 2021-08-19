8bfb92d4b543e11dfb4eabb5452e21bcdba07f1e
Tristan Angeles <setswipe@gmail.com>
2021-08-19 17:40:12 -0400

    recap_exercise_1 (completed)

--- recap_exercise_1/lib/problems.rb
+++ recap_exercise_1/lib/problems.rb
@@ -6,20 +6,30 @@
 #
 # all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
 def get_vowels(str)
+    result = ""
+    vowels ="aeiou"
+    str.each_char {|char| result+=char if vowels.include?(char)}
+    result
 
 end
 
 
 def all_vowel_pairs(words)
     vowels ="aeiou"
-    tempVowelArr = ""
+    check = ""
     result = []
 
     (0...words.length-1).each do |i|
-        tempVowelArr= get_vowels(word[i]) + get_vowels(words[i+1])
+        (i...words.length).each do |j|
+            check = get_vowels(words[i]) + get_vowels(words[j])
+            if check.include?("a") && check.include?("e") && check.include?("i") && check.include?("o") && check.include?("u")
 
+                result << words[i] + " " + words[j]
+            end
+        end
     end
 
+    result 
 
 end
 
@@ -32,6 +42,8 @@ end
 # composite?(9)     # => true
 # composite?(13)    # => false
 def composite?(num)
+    (2...num).each {|i| return true if num % i == 0}
+    false
 
 end
 
@@ -46,7 +58,9 @@ end
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
-
+result = []
+bigrams.each { |ele| result << ele if str.include?(ele)}
+result 
 end
 
 class Hash
@@ -64,6 +78,17 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
+        hsh = {}
+
+        prc ||= Proc.new { |k, v| k==v}
+
+        self.each do |k, v|
+            if prc.call(k,v)
+                hsh[k] = v
+            end
+        end
+
+        hsh
 
     end
 end
@@ -78,9 +103,21 @@ class String
     # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
     # "cats".substrings(2)  # => ["ca", "at", "ts"]
     def substrings(length = nil)
+        arr = []
+        if length !=nil
+            (0..self.length - length).each do |i|
+                arr<<self[i...i+length]
+            end
+        elsif
+            (1..self.length).each do |i|
+                arr<< substrings(i)
+            end
+        end
+        arr.flatten
 
     end
-
+#cats
+#ca -at -ts2
 
     # Write a method, String#caesar_cipher, that takes in an a number.
     # The method should return a new string where each char of the original string is shifted
@@ -92,6 +129,18 @@ class String
     # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
     # "zebra".caesar_cipher(4)    #=> "difve"
     def caesar_cipher(num)
+        alphabet = ("a".."z").to_a
+        new_idx = []
+
+        self.each_char do |char|
+            new_idx << (alphabet.index(char)+num) % 26
+        end
+
+        new_idx = new_idx.map do |ele|
+            alphabet[ele]
+        end
+
+        new_idx.join('')
 
     end
 end
